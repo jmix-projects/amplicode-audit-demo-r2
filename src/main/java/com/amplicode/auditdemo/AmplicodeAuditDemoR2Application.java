c272bf61754ca34f954a64553dafde731d3b50f8
Artem Glinov <a.glinov@haulmont.com>
2021-10-26 11:32:41 +0400

    Initial commit

---
+++ src/main/java/com/amplicode/auditdemo/AmplicodeAuditDemoR2Application.java
@@ -0,0 +1,13 @@
+package com.amplicode.auditdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplicodeAuditDemoR2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmplicodeAuditDemoR2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/amplicode/auditdemo/AmplicodeAuditDemoR2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amplicode.auditdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplicodeAuditDemoR2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
